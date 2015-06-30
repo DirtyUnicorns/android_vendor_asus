9e1b9b1fe9b956fd90a9313055fb0cd67e466b56
deadman96385 <seanhoyt963@gmail.com>
2015-06-29 23:57:24 -0400

    Remove last remaining moto crap
    Change-Id: Ib4fe718a1a9cb3b3fbc2c2abb465d25dfb28953c

--- moorefield/fhd/Android.mk
+++ moorefield/fhd/Android.mk
@@ -18,15 +18,5 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),fhd)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := MotGeoFenceSvc
-LOCAL_MODULE_OWNER := asus
-LOCAL_SRC_FILES := proprietary/app/$(LOCAL_MODULE)/$(LOCAL_MODULE).apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := platform
-include $(BUILD_PREBUILT)
-
 endif
 
